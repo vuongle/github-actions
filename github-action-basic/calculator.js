4a4c1fc0e51885d5d219a48c80da2d23e4989672
vuonglq <le.quang.vuong@quantic.com.vn>
2024-09-04 10:28:29 +0700

    add new code to feature branch

--- github-action-basic/calculator.js
+++ github-action-basic/calculator.js
@@ -12,5 +12,13 @@ function minus(numbers) {
         .reduce((a, b) => a - b)
 }
 
+function multiply(numbers) {
+    return numbers
+        .split(',')
+        .map(x => parseInt(x))
+        .reduce((a, b) => a * b)
+}
+
 exports.add = add;
 exports.minus = minus;
+exports.multiply = multiply;
